19b44ccfff010dcad62d4b09a8a9e01d4a1d49cb
Levin Mutai <levinmutai2@gmail.com>
2023-08-18 13:05:07 +0300

    added two spiders

--- trapper/spiders/cnn.py
+++ trapper/spiders/cnn.py
@@ -69,3 +69,51 @@ class CnnSportsSpider(scrapy.Spider):
                 "category": "sports",
                 "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),   
             }
+
+class CnnTechnologySpider(scrapy.Spider):
+    '''
+    spider to scrap all technology articles from cnn
+
+    '''
+    name = "cnn-technology"
+    allowed_domains = ["edition.cnn.com"]
+    start_urls = ["https://edition.cnn.com/technology"]
+
+    def parse(self, response):
+        articles = response.css("a.container__link")
+        
+        for article in articles:
+            headline = article.css("span::text").get()
+            print(headline)
+            link = article.css("a").attrib["href"]  
+            yield {
+                "headline": headline,
+                "link": link,
+                "source": "CNN",
+                "category": "technology",
+                "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),   
+            }
+
+class CnnHealthSpider(scrapy.Spider):
+    '''
+    spider to scrap all health articles from cnn
+
+    '''
+    name = "cnn-health"
+    allowed_domains = ["edition.cnn.com"]
+    start_urls = ["https://edition.cnn.com/health"]
+
+    def parse(self, response):
+        articles = response.css("a.container__link")
+        
+        for article in articles:
+            headline = article.css("span::text").get()
+            print(headline)
+            link = article.css("a").attrib["href"]  
+            yield {
+                "headline": headline,
+                "link": link,
+                "source": "CNN",
+                "category": "health",
+                "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),   
+            }
