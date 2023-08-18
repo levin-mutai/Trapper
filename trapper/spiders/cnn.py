ed4f9959e1dd7bed0c91136906193133a1511398
Levin Mutai <levinmutai2@gmail.com>
2023-08-18 13:02:01 +0300

    added CnnSports Spider

--- trapper/spiders/cnn.py
+++ trapper/spiders/cnn.py
@@ -25,7 +25,7 @@ class CnnGeneralSpider(scrapy.Spider):
 class CnnBusinessSpider(scrapy.Spider):
     '''
     spider to scrap all business articles from cnn
-    
+
     '''
     name = "cnn-business"
     allowed_domains = ["edition.cnn.com"]
@@ -43,5 +43,29 @@ class CnnBusinessSpider(scrapy.Spider):
                 "link": link,
                 "source": "CNN",
                 "category": "business",
+                "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),   
+            }
+
+class CnnSportsSpider(scrapy.Spider):
+    '''
+    spider to scrap all sports articles from cnn
+
+    '''
+    name = "cnn-sports"
+    allowed_domains = ["edition.cnn.com"]
+    start_urls = ["https://edition.cnn.com/sports"]
+
+    def parse(self, response):
+        articles = response.css("a.container__link")
+        
+        for article in articles:
+            headline = article.css("span::text").get()
+            print(headline)
+            link = article.css("a").attrib["href"]  
+            yield {
+                "headline": headline,
+                "link": link,
+                "source": "CNN",
+                "category": "sports",
                 "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),   
             }
