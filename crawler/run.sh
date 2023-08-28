a97aeb6adfdab7665e7a410930c995c9aa2481cc
Levin Mutai <levinmutai2@gmail.com>
2023-08-28 12:27:53 +0300

    modified run.sh

--- crawler/run.sh
+++ crawler/run.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# Get the directory name of the script being executed
+script_dir=$(dirname "$0")
 
-# Specify the directory path
+# Get the current working directory
+current_dir=$(pwd)
+
+# Compare the script directory with the current directory
+if [ "$script_dir" == "$current_dir" ]; then
+  echo "Script is being run from the same directory."
+else
+  echo "Changing to the required directory."
+  cd $script_dir
+fi
+
 logs_dir="./logs"
 
-# Check if the directory exists
+    # Check if logs directory exists
 if [ ! -d "$logs_dir" ]; then
     echo "Creating directory $logs_dir"
     mkdir "$logs_dir"
 fi
+
+
 # Start Scrapyd server in the background
 scrapyd > ./logs/scrapyd_logs.txt 2>&1 & 
 
@@ -19,3 +33,6 @@ scrapyd-deploy default
 scrapydweb  > ./logs/scrapydweb_logs.txt 2>&1 &
 
 cat ./logs/scrapyd_logs.txt
+
+cd $current_dir
+
