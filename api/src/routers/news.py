ad205028127744faf45c3359f4c4f4d0d6a6c0e5
Levin Mutai <levinmutai2@gmail.com>
2023-08-28 16:15:03 +0300

    Cleaned news APi

--- api/src/routers/news.py
+++ api/src/routers/news.py
@@ -18,49 +18,4 @@ background_tasks = BackgroundTasks()
 
 router = APIRouter(tags=["Wallet"], prefix="")
 
-router.get("/")
-def get_news(
-    
-    background_tasks: BackgroundTasks,
-    pagination:dict = Depends(get_pagination),
-    db: Session = Depends(
-        get_db,
-    ),
-    ):
-    """
-    Get news.
-    """
-    news = db.query(News).all()
-    
-    return paginate(res=news,pagination=pagination)  
 
-router.get("/categories")
-def get_category(
-    background_tasks: BackgroundTasks,
-    pagination:dict = Depends(get_pagination),
-    db: Session = Depends(
-        get_db,
-    ),
-    ):
-    
-    """
-    Get news.
-    """
-
-    return response(res="res")    
-router.get("/categories/{category}")
-def get_news_by_category(
-    category:str,
-
-    background_tasks: BackgroundTasks,
-    pagination:dict = Depends(get_pagination),
-    db: Session = Depends(
-        get_db,
-    ),
-    ):
-    """
-    Get news.
-    """
-    news = db.query(News).filter(News.category == category).all()
-    
-    return paginate(res=news,pagination=pagination)
