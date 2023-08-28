33ddaa7ece7c7c36795f76d2089c7b30d3acd9cc
Levin Mutai <levinmutai2@gmail.com>
2023-08-28 11:19:33 +0300

    updated routes

--- api/src/models/models.py
+++ api/src/models/models.py
@@ -3,7 +3,7 @@ from database import Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 
-class news(Base):
+class News(Base):
     __tablename__ = "news"
 
     id = Column(Integer, primary_key=True)
@@ -14,14 +14,14 @@ class news(Base):
     content = Column(String, nullable=True)
     source = Column(String)
 
-    def __init__(self, title, url, date, category, content, source):
-        self.title = title
-        self.url = url
-        self.date = date
-        self.category = category
-        self.content = content
-        self.source = source
+    # def __init__(self, title, url, date, category, content, source):
+    #     self.title = title
+    #     self.url = url
+    #     self.date = date
+    #     self.category = category
+    #     self.content = content
+    #     self.source = source
 
-    def __repr__(self):
-        return "<News(title='%s', url='%s', date='%s', category='%s', content='%s', source='%s')>" % (
-            self.title, self.url, self.date, self.category, self.content, self.source)
+    # def __repr__(self):
+    #     return "<News(title='%s', url='%s', date='%s', category='%s', content='%s', source='%s')>" % (
+    #         self.title, self.url, self.date, self.category, self.content, self.source)
